633dc98185dd9c2f050b9981994060f57b70368a
(عبد الرقيب)ABDUL RAQIB <35594183+16ug10001@users.noreply.github.com>
2018-12-05 12:26:32 +0530

    Add files via upload

---
+++ abdulaplet.java
@@ -0,0 +1,13 @@
+import java.applet.Applet;  
+import java.awt.Graphics;  
+public class abdulaplet extends Applet{  
+  
+public static void main paint(Graphics g){  
+g.drawString("welcome to applet",150,150);  
+}  
+  
+}  
+/* 
+<applet code="First.class" width="300" height="300"> 
+</applet> 
+*/   
